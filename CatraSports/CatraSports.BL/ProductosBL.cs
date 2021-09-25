140d2e44f02616a25a69d8d6dd9cdacb3c6a23cc
StevenPerdomo <wsperdomo@unah.hn>
2021-09-24 23:21:25 -0600

    Agregando Entity Framework Code First

--- CatraSports/CatraSports.BL/ProductosBL.cs
+++ CatraSports/CatraSports.BL/ProductosBL.cs
@@ -8,35 +8,19 @@ namespace CatraSports.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+        public List<Producto> ListadeProductos { get; set; }
+
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+            ListadeProductos = new List<Producto>();
+        }
+
         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Cinta Kinesiológica";
-            producto1.Precio = 150;
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Cuerda para saltar";
-            producto2.Precio = 100;
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Playera deportiva para hombre";
-            producto3.Precio = 200;
-
-            var producto4 = new Producto();
-            producto4.Id = 4;
-            producto4.Descripcion = "Playera deportiva para mujer";
-            producto4.Precio = 220;
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-            listadeProductos.Add(producto4);
-
-            return listadeProductos;
+            ListadeProductos = _contexto.Productos.ToList();
+            return ListadeProductos;
         }
     }
 }
