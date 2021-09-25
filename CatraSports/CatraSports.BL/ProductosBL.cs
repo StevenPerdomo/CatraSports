3223353288127f5d6d15bbfbecda284663c26273
StevenPerdomo <wsperdomo@unah.hn>
2021-09-24 20:48:54 -0600

    Agregando proyectos Web, Win y Libreria BL

---
+++ CatraSports/CatraSports.BL/ProductosBL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatraSports.BL
+{
+    public class ProductosBL
+    {
+        public List<Producto> ObtenerProductos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Cinta Kinesiológica";
+            producto1.Precio = 150;
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Cuerda para saltar";
+            producto2.Precio = 100;
+
+            var producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descripcion = "Playera deportiva para hombre";
+            producto3.Precio = 200;
+
+            var producto4 = new Producto();
+            producto4.Id = 4;
+            producto4.Descripcion = "Playera deportiva para mujer";
+            producto4.Precio = 220;
+
+            var listadeProductos = new List<Producto>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+            listadeProductos.Add(producto4);
+
+            return listadeProductos;
+        }
+    }
+}
